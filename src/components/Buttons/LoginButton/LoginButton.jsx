ac63bebf0fa1a3a10921b2532bfeb0ce9dca51ef
Sergei <mac@MacBook-Air-Mac.local>
2023-03-14 13:17:10 +0200

    Hotfix: fixed gh-pages refresh error and Login button styles

--- src/components/Buttons/LoginButton/LoginButton.jsx
+++ src/components/Buttons/LoginButton/LoginButton.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { useAuth } from 'hooks';
 import { PrimaryButton } from 'components';
+import { Wrapper } from './LoginButton.styled';
 
 const LoginButton = () => {
   const { handleLogin } = useAuth();
 
-  return <PrimaryButton onClick={handleLogin}>Log In</PrimaryButton>;
+  return (
+    <Wrapper>
+      <PrimaryButton onClick={handleLogin}>Log In</PrimaryButton>
+    </Wrapper>
+  );
 };
 
 export default LoginButton;
---
+++ src/components/Buttons/LoginButton/LoginButton.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  @media (max-width: 600px) {
+    margin-left: auto;
+  }
+`;
